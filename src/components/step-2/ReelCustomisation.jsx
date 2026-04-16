d2a6cb0387c0ab4034e64aaf4d1c966c7fd58e35
Max <max.ferchland@gmx.de>
2026-04-16 12:11:03 +0100

    feat: added a test-audio file, fixed the layout of the reel-image space to properly overlap each other and staying inside its container properly, added a music player list underneath the mentioned image container

--- src/components/step-2/ReelCustomisation.jsx
+++ src/components/step-2/ReelCustomisation.jsx
@@ -8,33 +8,68 @@ export default function ReelCustomisation() {
     {src: "src/assets/video.mp4", title: "Video 3", id: 3, type: "video"},
     {src: "src/assets/video.mp4", title: "Video 4", id: 4, type: "video"},
     {src: "src/assets/video.mp4", title: "Video 5", id: 5, type: "video"},
+    {src: "src/assets/video.mp4", title: "Video 6", id: 6, type: "video"},
+    {src: "src/assets/video.mp4", title: "Video 7", id: 7, type: "video"},
+    
   ]
+  const audioData = [
+    {src: "src/assets/video.mp4", title: "Video 1", id: 1, type: "video"},
+    {src: "src/assets/video.mp4", title: "Video 2", id: 2, type: "video"},
+    {src: "src/assets/video.mp4", title: "Video 3", id: 3, type: "video"},
+    {src: "src/assets/video.mp4", title: "Video 4", id: 4, type: "video"},
+    {src: "src/assets/video.mp4", title: "Video 5", id: 5, type: "video"},
+    {src: "src/assets/video.mp4", title: "Video 6", id: 6, type: "video"},
+    {src: "src/assets/video.mp4", title: "Video 7", id: 7, type: "video"},
+  ]
+  
   return (
     <div className="flex">
       <div className="flex-1">
         <h1>Reel Customisation</h1>
-        <div className="w-full max-h-140 p-2 m-4 flex items-center border border-slate-200 rounded-xl">
-          <div className="grid place-items-center w-[520px] h-[320px] rounded-xl border border-slate-200 overflow-hidden">
-  {mediaData.map((media, index) => (
-    <div
-      key={media.id}
-      className="col-start-1 row-start-1 w-full h-full rounded-xl overflow-hidden shadow-lg"
-      style={{
-        transform: `translate(${index * 18}px)`,
-        zIndex: index,
-      }}
-    >
-      <video src={media.src} className="w-full h-full object-cover" />
-    </div>
-  ))}
-</div>
-
+        {/* The div for changing the order of the images */} 
+        <div className="w-full p-2 m-4 flex border border-slate-200 rounded-xl">
+          <div className="flex items-center">
+            {mediaData.map((media, index) => (
+              <div
+                key={media.id}
+                className={`rounded-xl overflow-hidden hover:z-100 ${index !== 0 ? "-ml-40" : ""}`}
+                // style={{ zIndex: index }}
+              >
+                <video src={media.src} className=" object-cover" />
+              </div>
+            ))}
+          </div>
+        </div>
+          {/* The div for the media player */}
+          <div className="w-full p-2 m-4 flex flex-col items-center border border-slate-200 rounded-xl">
+            <h2 className="w-full">Select Music for the Video</h2>
+            <div className="w-full max-h-60 overflow-y-auto border:hidden">
+              <ul className="w-full h-auto flex gap-2 flex-col justify-center px-2 py-2">
+                {audioData.map((media) => (
+                  <li className="w-full">
+                    <audio controls className="w-full">
+                      <source src={media.src} title={media.title} type="audio/mpeg" />
+                      Your browser does not support the audio element.
+                    </audio>
+                  </li>
+                ))}
+              </ul>
+            </div>
         </div>
       </div>
       <div className="flex-1">
         <h2>Preview</h2>
-        <div className="w-full h-full p-2 m-4 border border-slate-200 rounded-xl">
-          <p>Preview area</p>
+        <div className="w-full p-2 m-4 flex border border-slate-200 rounded-xl">
+          
+        </div>
+        <div className="w-full p-2 m-4 flex border border-slate-200 rounded-xl">
+
+        </div>
+      </div>
+      <div>
+        <h2>Preview2</h2>
+        <div className="w-full h-full p-2 m-4 flex border border-slate-200 rounded-xl">
+          <p>Preview area 2</p>
         </div>
       </div>
     </div>
