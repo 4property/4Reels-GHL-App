e5bef0acd5940121d9738e829a939c5dbf32ca57
Max <max.ferchland@gmx.de>
2026-04-17 16:01:12 +0100

    style: added icons to the buttons (still have to be changed, so thy are always at the right end of the button), started to layout the Review Area, moved buttons outside of review area as per figma-design

--- src/components/step-5/Preview.jsx
+++ src/components/step-5/Preview.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import videoFile from "../../assets/video.mp4";
 import Button from "../utils/Button";
+import { MdOutlineFileUpload } from "react-icons/md";
+import { FiClock } from "react-icons/fi";
 
 export default function Preview() {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Preview</h1>
-      <div className="h-full flex  items-center justify-evenly">
+      <div className="h-full flex items-center justify-evenly">
         <div className="w-100">
           <video src={videoFile} controls className="rounded-xl" />
         </div>
-        <div className="w-100 h-auto rounded-xl bg-gray-200 flex flex-col items-center justify-center">
-          <p className="text-gray-500">Preview Area</p>
-          <Button>Upload now</Button>
-          <Button>Schedule upload</Button>
-        </div>
+        <div className="flex flex-col">
+          <div className="w-100 h-auto rounded-xl bg-gray-200 flex flex-col items-center justify-center">
+            <p className="text-gray-500 flex">Preview Area</p>
+          </div>
+          <Button className="flex flex-row items-center justify-center">
+              <span className="flex items-center gap-2">
+                Upload now
+                <MdOutlineFileUpload />
+              </span>
+            </Button>
+            <Button className="flex">
+              <span className="flex items-center gap-2">
+                Schedule upload
+                <FiClock />
+              </span>
+            </Button>
+          </div>
       </div>
     </div>
   );
