a6e7cf01ce8d69e1d8412963cd254c2bdbed69fb
Max <max.ferchland@gmx.de>
2026-04-17 11:10:35 +0100

    style: added style for the drag and drop fields, added Icon to button; feat: added drag and drop functionality for music player and for the images/videos (added music shows up at the front, added images/videos show up at the back) cant be saved yet though

--- src/components/step-2/ReelCustomisation.jsx
+++ src/components/step-2/ReelCustomisation.jsx
@@ -1,8 +1,10 @@
 import React,{ useState } from "react";
 import Button from "../utils/Button";
+import {FaBookmark} from "react-icons/fa";
+import { GoPlusCircle } from "react-icons/go";
 
 export default function ReelCustomisation() {
-  const mediaData = [
+  const [mediaData, setMediaData] = useState([
     // Just placeholder video data, even if most of these things should be images. only used for the measurements and site-layout
     { src: "src/assets/video.mp4", title: "Video 1", id: 1, type: "video" },
     { src: "src/assets/video.mp4", title: "Video 2", id: 2, type: "video" },
@@ -11,35 +13,65 @@ export default function ReelCustomisation() {
     { src: "src/assets/video.mp4", title: "Video 5", id: 5, type: "video" },
     { src: "src/assets/video.mp4", title: "Video 6", id: 6, type: "video" },
     { src: "src/assets/video.mp4", title: "Video 7", id: 7, type: "video" },
-  ];
-  const audioData = [
-    { src: "src/assets/video.mp4", title: "Video 1", id: 1, type: "video" },
-    { src: "src/assets/video.mp4", title: "Video 2", id: 2, type: "video" },
-    { src: "src/assets/video.mp4", title: "Video 3", id: 3, type: "video" },
-    { src: "src/assets/video.mp4", title: "Video 4", id: 4, type: "video" },
-    { src: "src/assets/video.mp4", title: "Video 5", id: 5, type: "video" },
-    { src: "src/assets/video.mp4", title: "Video 6", id: 6, type: "video" },
-    { src: "src/assets/video.mp4", title: "Video 7", id: 7, type: "video" },
-  ];
+  ]);
+  const [audioData, setAudioData] = useState([
+    { src: "src/assets/artmylife-powerful-dramatic-trailer-514242.mp3", title: "Audio 1", id: 1, type: "audio" },
+    { src: "src/assets/artmylife-powerful-dramatic-trailer-514242.mp3", title: "Audio 2", id: 2, type: "audio" },
+    { src: "src/assets/artmylife-powerful-dramatic-trailer-514242.mp3", title: "Audio 3", id: 3, type: "audio" },
+    { src: "src/assets/artmylife-powerful-dramatic-trailer-514242.mp3", title: "Audio 4", id: 4, type: "audio" },
+    { src: "src/assets/artmylife-powerful-dramatic-trailer-514242.mp3", title: "Audio 5", id: 5, type: "audio" },
+    { src: "src/assets/artmylife-powerful-dramatic-trailer-514242.mp3", title: "Audio 6", id: 6, type: "audio" },
+    { src: "src/assets/artmylife-powerful-dramatic-trailer-514242.mp3", title: "Audio 7", id: 7, type: "audio" },
+  ]);
 
   const [isToggled1, setIsToggled1] = useState(false);
-  const [droppedItem, setDroppedItem] = useState(null);
   const [caption, setCaption] = React.useState("");
   const handleCaptionChange = (e) => {
     setCaption(e.target.value);
   }
 
   // started implementing drag and drop functionality, but stopped, because we need to connect to server for it to work, doesnt make sense to try doing it beforehand, as we would need to change it anyway when connecting to the server
-  function handleDragOver(e) {
+  function handleDragOverAudio(e) {
     e.preventDefault();
   }
-  function handleDrop(e) {
+  function handleDropAudio(e) {
     e.preventDefault();
 
     const item = Array.from(e.dataTransfer.files);
-    setDroppedItem(item);
-    // {item.type === "video/mp4" ? (mediaData.):()}
+    // setDroppedItem(item);
+    item.forEach((file) => {
+      if(file.type.startsWith("audio/")) {
+        const newAudioItem = {
+          src: URL.createObjectURL(file),
+          title: file.name,
+          id: audioData.length + 1,
+          type: "audio",
+        };
+        setAudioData((prev) => [newAudioItem, ...prev]);
+      }
+    });
   }
+  function handleDragOverMedia(e) {
+    e.preventDefault();
+  }
+  function handleDropMedia(e) {
+    e.preventDefault();
+
+    const item = Array.from(e.dataTransfer.files);
+    // setDroppedItem(item);
+    item.forEach((file) => {
+      if(file.type.startsWith("video/") || file.type.startsWith("image/")) {
+        const newMediaItem = {
+          src: URL.createObjectURL(file),
+          title: file.name,
+          id: mediaData.length + 1,
+          type: file.type.startsWith("video/") ? "video" : "image",
+        };
+        setMediaData((prev) => [...prev, newMediaItem]);
+      }
+    });
+  }
+
   return (
     <div className="flex">
       <div className="flex flex-col gap-4 *:gap-4">
@@ -54,13 +86,16 @@ export default function ReelCustomisation() {
                   className={`rounded-xl cursor-pointer overflow-hidden hover:z-100 hover:duration-200 hover:scale-105 ${index !== 0 ? "-ml-40" : ""}`}
                   // style={{ zIndex: index }}
                 >
-                  <video src={media.src} className=" object-cover" />
+                  <video src={media.src} title={media.title} className=" object-cover" />
                 </div>
               ))}
             </div>
           </div>
-          <div className="w-full flex-1 p-2 flex border border-slate-200 rounded-xl" onDragOver={handleDragOver} onDrop={handleDrop}>
-            test
+          <div className="w-full p-2 flex flex-1 flex-col items-center border border-slate-200 rounded-xl" onDragOver={handleDragOverMedia} onDrop={handleDropMedia}>
+            <p className="flex h-full items-center">Upload your own Images or Videos</p>
+            <span className="flex flex-col h-full items-center gap-2">
+              <GoPlusCircle />
+            </span>
           </div>
           {/* The div for the media player */}
         </div>
@@ -84,8 +119,11 @@ export default function ReelCustomisation() {
               </ul>
             </div>
           </div>
-          <div className="w-full p-2 flex flex-1 border border-slate-200 rounded-xl">
-            test2
+          <div className="w-full p-2 flex flex-1 flex-col items-center border border-slate-200 rounded-xl" onDragOver={handleDragOverAudio} onDrop={handleDropAudio}>
+            <p className="flex h-full items-center">Upload your Music</p>
+            <span className="flex flex-col h-full items-center gap-2">
+              <GoPlusCircle />
+            </span>
           </div>
         </div>
       </div>
@@ -93,13 +131,16 @@ export default function ReelCustomisation() {
         <div className=" h-3/5 p-2 ml-4 mt-10 flex flex-col border border-slate-200 rounded-xl">
           <h2 className="ml-4">Change Captions:</h2>
           <div className="w-full h-full flex border border-slate-200 rounded-xl">
-            <textarea
+            {isToggled1 ? 
+            (<p className="p-2 font-bold">Caption Disabled. Use the voiceover feature to generate matching captions to your spoken voiceover.</p>)
+            : 
+            (<textarea
               value={caption}
               onChange={handleCaptionChange}
               disabled={isToggled1}
               placeholder={isToggled1 ? "Speech-to-text is active" : "Enter caption here..."}
               className="w-full h-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-950 disabled:bg-gray-100"
-            />
+            />)}
           </div>
         </div>
         <label className="inline-flex items-center cursor-pointer p-2 m-4 min-w-60">
@@ -119,14 +160,17 @@ export default function ReelCustomisation() {
           >
             <span
               className={`absolute left-1 top-1 w-4 h-4 bg-white rounded-full transition-transform duration-200 ease-in-out ${
-                isToggled1 ? "translate-x-4" : "translate-x-0"
+                isToggled1 ? "translate-x-3" : "translate-x-0"
               }`}
             ></span>
           </div>
         </label>
         <div className="ml-4 mt-6 flex justify-center">
           <Button>
-            Set default Configuration
+            <span className="flex items-center gap-2">
+              <p>Set default Configuration</p>
+              <FaBookmark className="ml-2" />
+            </span>
           </Button>
         </div>
       </div>
